715973d8e46afec3122ee34be67de29e56f15cc6
RyuichiMatsuda <54462227+RyuichiMatsuda@users.noreply.github.com>
2022-02-09 13:26:42 +0900

    view修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -105,10 +105,10 @@
                                         <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
                                         <polyline points="9 22 9 12 15 12 15 22"></polyline>
                                     </svg>
-                                    <span class="ml-2">Dashboard</span>
+                                    <span class="ml-2">インシデント</span>
                                 </a>
                             </li>
-                            <li class="nav-item">
+                            {{-- <li class="nav-item">
                                 <a class="nav-link" href="#">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -168,7 +168,7 @@
                                     </svg>
                                     <span class="ml-2">Integrations</span>
                                 </a>
-                            </li>
+                            </li> --}}
 
                         </ul>
 
